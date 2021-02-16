53e6dba1f6687b94cf998125cf815a75a3fd540f
Gelson Gomes Rodrigues <geoldery@gmail.com>
2021-02-15 23:39:11 -0300

    images_avro

--- hello_avro_cpp/source/Application.hpp
+++ hello_avro_cpp/source/Application.hpp
@@ -7,24 +7,17 @@ class Application
 {
     public:
     Application() {}
-    void create_trades()
-    {
-    }
+    void create_trades(){}
 
-    void init()
-    {
-    }
+    void init(){}
     void run()
     {
-
         bitcoin::Trade trade("8615953", "1613358560", "buy", "263249.83999", "0.03000899");
         trade.serialize();
         trade.deserialize();
     }
 
-    void shutdown()
-    {
-    }
+    void shutdown(){}
 };
 
 #endif
